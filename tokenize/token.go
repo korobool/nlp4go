9dd0ec2b63f8d60329588796df944e572fe4265f
Dmytro Novikov <novikov.dmytro@gmail.com>
2015-12-09 18:59:05 +0200

    Added json tags for SentenceToken

--- tokenize/token.go
+++ tokenize/token.go
@@ -1,13 +1,17 @@
 package tokenize
 
 type SentenceToken struct {
-	Text          []rune
-	PosStart      int
-	PosEnd        int
-	IsQuoteStart  bool
-	IsQuoteEnd    bool
-	IsEllipsis    bool
-	HasApostrophe bool
+	Text          []rune `json:"text"`
+	PosStart      int    `json:"pos_start"`
+	PosEnd        int    `json:"pos_end"`
+	IsQuoteStart  bool   `json:"is_quote_start"`
+	IsQuoteEnd    bool   `json:"is_quote_end"`
+	IsEllipsis    bool   `json:"is_ellipsis"`
+	HasApostrophe bool   `json:"has_apostrophe"`
+}
+
+func (t *SentenceToken) String() string {
+	return string(t.Text)
 }
 
 func NewSentenceToken(str []rune, posStart, posEnd int) *SentenceToken {
