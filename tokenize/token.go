5c89415795e08aa196cf5359831b15f603a688d7
Dmytro Novikov <novikov.dmytro@gmail.com>
2015-12-11 16:10:05 +0200

    Add comparing method for SentenceToken

--- tokenize/token.go
+++ tokenize/token.go
@@ -1,5 +1,7 @@
 package tokenize
 
+import "fmt"
+
 type SentenceToken struct {
 	Text          []rune `json:"text"`
 	PosStart      int    `json:"pos_start"`
@@ -10,14 +12,45 @@ type SentenceToken struct {
 	HasApostrophe bool   `json:"has_apostrophe"`
 }
 
-func (t *SentenceToken) String() string {
-	return string(t.Text)
-}
-
 func NewSentenceToken(str []rune, posStart, posEnd int) *SentenceToken {
 	return &SentenceToken{
 		Text:     str[posStart:posEnd],
 		PosStart: posStart,
 		PosEnd:   posEnd,
 	}
+}
+
+func (t *SentenceToken) String() string {
+	return fmt.Sprintf("text=%s start=%d end=%d q_s=%v q_e=%v e=%v a=%v",
+		string(t.Text),
+		t.PosStart,
+		t.PosEnd,
+		t.IsQuoteStart,
+		t.IsQuoteEnd,
+		t.IsEllipsis,
+		t.HasApostrophe,
+	)
+}
+
+func (t *SentenceToken) Equals(compare *SentenceToken) bool {
+	if string(t.Text) != string(compare.Text) {
+		return false
+	}
+	if t.PosStart != compare.PosStart {
+		return false
+	}
+	if t.PosEnd != compare.PosEnd {
+		return false
+	}
+	if t.IsQuoteStart != compare.IsQuoteStart {
+		return false
+	}
+	if t.IsEllipsis != compare.IsEllipsis {
+		return false
+	}
+	if t.IsQuoteEnd != compare.IsQuoteEnd {
+		return false
+	}
+
+	return true
 }
